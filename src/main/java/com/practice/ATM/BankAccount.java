fea5e7709b3ea666c54080cd3c9d0f3eab57954e
Gavin T Valencia <Gavin.Valencia@ey.com>
2023-04-25 18:30:22 -0400

    fixed issue with lambok

--- src/main/java/com/practice/ATM/BankAccount.java
+++ src/main/java/com/practice/ATM/BankAccount.java
@@ -3,36 +3,18 @@ package com.practice.ATM;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.*;
 
 import java.util.Objects;
-
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 @Entity
-class BankAccount {
+public @Data class BankAccount {
 
     private @Id @GeneratedValue Long id;
-    private Double accountBalance;
+    private @NonNull Double accountBalance;
 
-    BankAccount(){}
 
-    public BankAccount(Double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getAccountBalance() {
-        return accountBalance;
-    }
-
-    public void setAccountBalance(Double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
     public void deposit(Double depositAmount){
         this.accountBalance += depositAmount;
     }
@@ -40,24 +22,4 @@ class BankAccount {
         this.accountBalance -= withdrawAmount;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BankAccount that = (BankAccount) o;
-        return Objects.equals(id, that.id)  && Objects.equals(accountBalance, that.accountBalance);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, accountBalance);
-    }
-
-    @Override
-    public String toString() {
-        return "BankAccount{" +
-                "id=" + id +
-                ", accountBalance=" + accountBalance +
-                '}';
-    }
 }
